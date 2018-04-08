b516517dc804da785c7a43f1930fec23f8788b2b
Rizuwan <1430814@iub.edu.bd>
2018-04-08 17:43:24 +0600

    lib

--- librarian.js
+++ librarian.js
@@ -3,35 +3,50 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
-  /* Task 2 */
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" }
+
 ];
 
 /* Task 1 */
 function getBookTitle( index ){
-  //add code
+  return library[index].title;
 }
 
-console.log(getBookTitle(3));
+console.log(getBookTitle(2));
 
-/* Task 3 */
+//* Task 3 *//
 function addBook( book ){
-  //add code
+  library.push(book);
+  return library;
 }
 
-var newBook = {};
+var newBook = {title: "title ", year: "year", author: "author ", publisher: "publisher"};
 console.log(addBook(newBook));
 
 /* Task 4 */
 function bookByAuthor(){
-  //add code
+  var newLibrary=library;
+  newLibrary.sort(function (a,b){
+  return a.author.localeCompare(b.author);
+  });
+  return newLibrary;
 }
 
 console.log(bookByAuthor());
 
 /* Task 5 */
 function findByTitle( title ){
-  //add code
+  var value;
+  var n;
+  var found=[];
+
+  for (var i= 0; i<library.length; i++){
+    value = library[i].title;
+    n = value.toLowerCase().includes(title);
+    if (n) found.push(library[i]);
+  }
+  return found;
 }
 
 console.log(findByTitle("man"));
